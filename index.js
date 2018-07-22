0f9a8ae5372ed45c2409dbbfb673576c636b1546
ajai1 <emailtoajai@gmail.com>
2018-07-22 23:04:03 +0530

    commiting request param

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/api/firstpage', (req, res) => {
     res.send('First Page Response');
 });
 
+app.get('/api/new/:id/:name', (req, res)=>{
+  res.send(req.query);
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () =>{
     console.log(`Listening to the port ${port}`);
