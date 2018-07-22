dece431b866a57343a1a422e4eac9525bf1a18dc
ajai1 <emailtoajai@gmail.com>
2018-07-23 00:02:35 +0530

    using joi to validate post request

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const Joi = require('joi');
 const app = express();
+app.use(express.json());
 
 const users = [
     {id:1, name:'Ajai'},
@@ -11,8 +13,8 @@ app.get('/', (req, res) => {
     res.send('Hey Ajai');
 });
 
-app.get('/api/firstpage', (req, res) => {
-    res.send('First Page Response');
+app.get('/api/users', (req, res) => {
+    res.send(users);
 });
 
 app.get('/api/users/:id', (req, res)=>{
@@ -22,6 +24,27 @@ app.get('/api/users/:id', (req, res)=>{
   }else{
       res.send(user);
   }
+});
+
+app.post('/api/users', (req, res) =>{
+
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    const result =  Joi.validate(req.body, schema);
+
+    if(result.error){
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+    
+    const user = {
+        id: users.length + 1,
+        name: req.body.name
+    };
+    users.push(user);
+    res.send(user);
 });
 
 const port = process.env.PORT || 3000;
