2de4e27dde7976e5818aecc7cf0f2243c98dfbb8
ajai1 <emailtoajai@gmail.com>
2018-07-22 23:27:13 +0530

    iterating through objects and finding the user

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+const users = [
+    {id:1, name:'Ajai'},
+    {id:2, name:'Kumar'},
+    {id:3, name:'Krishna'}
+];
+
 app.get('/', (req, res) => {
     res.send('Hey Ajai');
 });
@@ -9,8 +15,13 @@ app.get('/api/firstpage', (req, res) => {
     res.send('First Page Response');
 });
 
-app.get('/api/new/:id/:name', (req, res)=>{
-  res.send(req.query);
+app.get('/api/users/:id', (req, res)=>{
+  const user = users.find(c => c.id === parseInt(req.params.id));
+  if(!user){
+      res.status(404).send('User not found');
+  }else{
+      res.send(user);
+  }
 });
 
 const port = process.env.PORT || 3000;
