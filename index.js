70e313d5c0d0bbb59c54e368c185ad08823aa7a4
ajai1 <emailtoajai@gmail.com>
2018-07-23 16:13:32 +0530

    builtIn Express Middleware

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const Joi = require('joi');
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.static('public'));
 
 const users = [
     {id:1, name:'Ajai'},
